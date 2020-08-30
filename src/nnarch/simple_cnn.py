623c118c396121c0e52abebc8660d436dc01e7d0
PajarKharisma <pajarkharisma@hotmail.com>
2020-08-30 15:23:38 +0700

    change arch

--- src/nnarch/simple_cnn.py
+++ src/nnarch/simple_cnn.py
@@ -13,11 +13,14 @@ class ConvNet(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.fc = nn.Linear(7*7*32, num_classes)
         
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
+        out = self.avgpool(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
         return out
