35a33c3b1d13a212deff499e58d71f567db2cf9f
PajarKharisma <pajarkharisma@hotmail.com>
2020-08-30 14:46:17 +0700

    update parameter

--- src/util/metrics.py
+++ src/util/metrics.py
@@ -5,14 +5,16 @@ from torch.autograd import Variable
 import copy
 import numpy as np
 
+from src.config.param import *
+
 def get_acc(model, dataset):
     model.eval()
     with torch.no_grad():
         correct = 0
         total = 0
         for images, labels in dataset:
-            images = images.to(device)
-            labels = labels.to(device)
+            images = images.to(Param.device)
+            labels = labels.to(Param.device)
             
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
@@ -26,8 +28,8 @@ def get_loss(model, dataset, criterion):
     total_loss = 0
     with torch.no_grad():
         for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device)
-            labels = labels.to(device)
+            images = images.to(Param.device)
+            labels = labels.to(Param.device)
             
             outputs = model(images)
             loss = criterion(outputs, labels).item()
