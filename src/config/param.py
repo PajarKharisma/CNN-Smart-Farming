5d6ca211c3a5ea48264be4056015de3424e1c879
PajarKharisma <pajarkharisma@hotmail.com>
2020-08-30 15:37:38 +0700

    change arch

--- src/config/param.py
+++ src/config/param.py
@@ -6,8 +6,8 @@ import numpy as np
 class Param():
     #Hyperparameter
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    batch_size = 23
-    number_epochs = 5
+    batch_size = 32
+    number_epochs = 2
     data_split = 0.9
     input_size = (300, 300)
     pretrained = False
