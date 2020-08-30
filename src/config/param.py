29abdf12e3d832228cc8112bba8b578f2c76761b
PajarKharisma <pajarkharisma@hotmail.com>
2020-08-30 14:42:46 +0700

    update

--- src/config/param.py
+++ src/config/param.py
@@ -6,8 +6,8 @@ import numpy as np
 class Param():
     #Hyperparameter
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    batch_size = 16
-    number_epochs = 100
+    batch_size = 8
+    number_epochs = 50
     data_split = 0.9
     input_size = (331, 331)
     pretrained = False
