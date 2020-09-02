b0dbde1f585c4d9c025762c9a8a32aa8d1f0e9ad
PajarKharisma <pajarkharisma@hotmail.com>
2020-09-03 01:23:50 +0700

    add 100 epoch

--- src/config/param.py
+++ src/config/param.py
@@ -7,7 +7,7 @@ class Param():
     #Hyperparameter
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     batch_size = 8
-    number_epochs = 50
+    number_epochs = 100
     data_split = 0.9
     input_size = (331, 331)
     pretrained = False
